8f3ce7833866035e206bb3f506f1cab8409cc01a
Myhre <nmyhre@fastx01.divms.uiowa.edu>
2018-05-17 08:19:43 -0500

    Added some bug notices

--- SuffixTree.java
+++ SuffixTree.java
@@ -1,14 +1,29 @@
 /** Efficient, fast implementation of suffix tree data type
+ *  I would like to make this a lazy implementation, ideally.
  */
 
-class Node {
+import java.util.LinkedList;
 
+/** A Node contains a list of Edges.
+ */
+class Node {
+    //Bug: What type of data structure could better hold the list of Edges?
+    public LinkedList edges;
+    //Bug: How to initialize the Node?
 }
 
+/** Edges contain prefixes, and connect a Node to another Node,
+ *  or a Leaf.
+ */
 class Edge {
-
+    public String prefix;
+    public Node incoming;
+    public Node outgoing;
+    //Bug: how to initialize th eEdge?
 }
 
 class SuffixTree {
-
+    //Bug: How to build the suffix tree brute force?
+    //Bug: How to build the suffix tree Efficiently?
+    //Bug: Save root node?
 }
